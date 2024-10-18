5b05d911a0e4c9abaf78303f91cf2204f6eeef48
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-10-18 20:53:52 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.EntityLayer.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategorysStatus { get; set; }
+    }
+}
