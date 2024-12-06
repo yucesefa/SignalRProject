12bd7fd94e2c7e242b7bcffb4ee3fdc6ed26f53f
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-06 20:44:44 +0300

    current message's done

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -123,5 +123,9 @@ namespace SignalRApi.Hubs
             var value = _menuTableService.TGetListAll();
             await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
         }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
