27a7940566ed09c2adf999361d79b1359e1bb6d7
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-13 19:16:29 +0300

    discount and slider revised

--- SignalRWebUI/Controllers/DiscountController.cs
+++ SignalRWebUI/Controllers/DiscountController.cs
@@ -78,6 +78,19 @@ namespace SignalRWebUI.Controllers
             }
             return View();
         }
+		public async Task<IActionResult> ChangeStatusToTrue(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7050/api/Discounts/ChangeStatusToTrue/{id}");
+			return RedirectToAction("Index");
+		}
 
-    }
+		public async Task<IActionResult> ChangeStatusToFalse(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7050/api/Discounts/ChangeStatusToFalse/{id}");
+			return RedirectToAction("Index");
+		}
+
+	}
 }
