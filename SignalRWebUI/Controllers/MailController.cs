777ce9629db585dfd5be8e892618267dcc5753ba
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-14 22:49:15 +0300

    qr,settings and mail operations done

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,41 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDtos;
+
+
+namespace SignalRWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddress = new MailboxAddress("SignalR Rezervasyon","eraybctv@gmail.com");
+            mimeMessage.From.Add(mailboxAddress);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User",createMailDto.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder(); 
+            bodyBuilder.TextBody = createMailDto.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject=createMailDto.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com",587,false);
+            client.Authenticate("eraybctv@gmail.com", "cumi ztth jvls xlbg");
+
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return RedirectToAction("Index","Category");
+        }
+    }
+}
