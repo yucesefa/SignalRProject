463d0eeee60bbb8f48997f57210a28352401f7db
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-16 17:00:12 +0300

    Procedures regarding customer table selection

--- SignalRApi/Controllers/MenuTablesController.cs
+++ SignalRApi/Controllers/MenuTablesController.cs
@@ -66,5 +66,18 @@ namespace SignalRApi.Controllers
             var value = _menuTableService.TGetById(id);
             return Ok(_mapper.Map<GetMenuTableDto>(value));
         }
+        [HttpGet("ChangeMenuTableStatusToTrue")]
+        public IActionResult ChangeMenuTableStatusToTrue(int id)
+        {
+            _menuTableService.TChangeMenuTableStatusToTrue(id);
+            return Ok("İşlem başarılı");
+        }
+
+        [HttpGet("ChangeMenuTableStatusToFalse")]
+        public IActionResult ChangeMenuTableStatusToFalse(int id)
+        {
+            _menuTableService.TChangeMenuTableStatusToFalse(id);
+            return Ok("İşlem başarılı");
+        }
     }
 }
