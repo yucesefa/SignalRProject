00cc30c2458339227cc8a6fe769f16d5d8438c54
Safa Yüce <48711705+yucesefa@users.noreply.github.com>
2024-12-16 15:13:43 +0300

    rapid api

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "08d4297573mshf4f79e4627fbc46p18f620jsnf86ecf342d7e" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var root = JsonConvert.DeserializeObject<RootTastyApi>(body);
+                var values = root.Results;
+                return View(values.ToList());
+            }
+            
+        }
+    }
+}
